c7903be6360ca0e46eaa8c1bcc29064c2ad727e2
Walter Zhang <walter.zhangfang@gmail.com>
2023-12-03 15:17:18 +1300

    update

---
+++ SeleniuTest/src/Day1.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.setProperty("webdriver.chrome.driver", "/Users/walter/Code/98selenium/bin/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.quit();
+    }
+}
